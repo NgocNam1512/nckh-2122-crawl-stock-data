1f6d3d233c63ef9d74b51b885f29dcd3334b5af7
namnn12 <v.namnn12@vinbigdata.org>
2022-02-08 01:11:44 +0700

    filter date in 1 day and need at least 2 word when filter content

--- preprocess_src/filter.py
+++ preprocess_src/filter.py
@@ -7,7 +7,7 @@ def filter_date(df):
     df_filter["event_id"] = 0
     date_df = pd.read_csv(date_path, sep="\t")
     for date in date_df.itertuples():
-        df_filter.loc[(df_filter["date"] > date.From) & (df_filter["date"] < date.To), "event_id"]=date.STT
+        df_filter.loc[(df_filter["date"] >= date.From) & (df_filter["date"] <= date.To), "event_id"]=date.STT
 
     return df_filter[df_filter['event_id']!=0]
 
@@ -20,6 +20,13 @@ def filter_content(df):
     is_covid_news = list()
     df_filter = df.copy()
     for row in df_filter.itertuples():
-        is_covid_news.append(any(ele in row.content.lower() for ele in list_word))
+        count_word = 0
+        for word in list_word:
+            if word in row.content.lower():
+                count_word += 1
+        if count_word >= 2:
+            is_covid_news.append(True)
+        else:
+            is_covid_news.append(False)
 
     return df_filter[is_covid_news]
